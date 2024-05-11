3b37759bc5b8a3859b5d70603dd6952437f82e8a
Oleksandr Zinchenko <zinchenko@live.com>
2024-05-11 14:11:21 -0400

    Organize deploy into module

--- src/nimbuscli/core/deploy/docker.py
+++ src/nimbuscli/core/deploy/docker.py
@@ -1,65 +1,11 @@
 from __future__ import annotations
 
 import logging
-from abc import ABC, abstractmethod
-from datetime import timedelta
-from functools import reduce
 
 from logdecorator import log_on_end, log_on_start
 
-from nimbuscli.core.runner import CompletedProcess, Runner
-
-
-class Service(ABC):
-    """
-    Defines an abstract service.
-    All services should follow the APIs defined by this class.
-    """
-
-    def __init__(self, name: str):
-        self._name: str = name
-
-    def __str__(self) -> str:
-        return self.name
-
-    @property
-    def name(self) -> str:
-        """
-        Service name.
-        """
-        return self._name
-
-    @abstractmethod
-    def start(self) -> OperationStatus:
-        """
-        Start the service.
-        """
-
-    @abstractmethod
-    def stop(self) -> OperationStatus:
-        """
-        Stop the service.
-        """
-
-
-class OperationStatus:
-    """
-    The outcome of the service operation.
-    """
-
-    def __init__(self, service: str, operation: str, kind: str):
-        self.service: str = service
-        self.operation: str = operation
-        self.kind: str = kind
-        self.processes: list[CompletedProcess] = []
-
-    @property
-    def success(self) -> bool:
-        return all(proc.success for proc in self.processes)
-
-    @property
-    def elapsed(self) -> timedelta:
-        return reduce(lambda a, b: a + b.elapsed, self.processes, timedelta(seconds=0))
+from nimbuscli.core.deploy.service import OperationStatus, Service
+from nimbuscli.core.runner import Runner
 
 
 class DockerService(Service):
---
+++ tests/core/deploy/test_service.py
@@ -0,0 +1,72 @@
+from datetime import timedelta as td
+
+import pytest
+
+from nimbuscli.core import OperationStatus
+
+
+class MockProc:
+
+    def __init__(self, s=None, e=None):
+        self._success = s
+        self._elapsed = e
+
+    @property
+    def success(self):
+        return self._success
+
+    @property
+    def elapsed(self):
+        return self._elapsed
+
+
+class TestOperationStatus:
+
+    @pytest.mark.parametrize(
+        "statuses",
+        [
+            [True],
+            [False],
+            [True, True, True],
+            [True, False, True, False],
+            [False, False, False],
+        ],
+    )
+    def test_success(self, statuses):
+        operation = OperationStatus("", "", "")
+        for s in statuses:
+            operation.processes.append(MockProc(s=s))
+
+        assert operation.success == all(statuses)
+
+    @pytest.mark.parametrize(
+        ["durations", "expected"],
+        [
+            [
+                [td(minutes=10)],
+                td(minutes=10),
+            ],
+            [
+                [td(minutes=10)] * 1000,
+                td(minutes=10_000),
+            ],
+            [
+                [td(minutes=10), td(minutes=3)],
+                td(minutes=13),
+            ],
+            [
+                [td(hours=1), td(minutes=10), td(seconds=3)],
+                td(hours=1, minutes=10, seconds=3),
+            ],
+            [
+                [td(minutes=17), td(minutes=10), td(seconds=3)],
+                td(minutes=27, seconds=3),
+            ],
+        ],
+    )
+    def test_elapsed(self, durations, expected):
+        operation = OperationStatus("", "", "")
+        for d in durations:
+            operation.processes.append(MockProc(e=d))
+
+        assert operation.elapsed == expected
